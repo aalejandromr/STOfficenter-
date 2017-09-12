a2fe888b9efae4fe4e1c8f78148280e2a2793ab4
Alejandro Mejia <alejandromejia@Alejandros-MacBook-Air.local>
2017-09-12 00:36:19 -0600

    Delete remove_direccions_id_from_clients.rb

--- db/migrate/20170912023451_remove_direccions_id_from_clients.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDireccionsIdFromClients < ActiveRecord::Migration
-  def up
-  	#remove_foreign_key :clients, name: "fk_rails_731c1ea404"
-  	remove_columns :clients, :direccions_id
-  end
-end
