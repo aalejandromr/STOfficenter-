dacde61cead425d2eef9ee9976026cec79bed223
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2017-09-12 13:55:39 -0600

    Add missing fields

--- db/migrate/20170912022812_add_missing_fields_to_clients.rb
+++ db/migrate/20170912022812_add_missing_fields_to_clients.rb
@@ -1,13 +1,13 @@
 class AddMissingFieldsToClients < ActiveRecord::Migration
   def change
-  	#add_column :clients, :nombre_comercial, :string
-  	#add_column :clients, :registro_fiscal, :string
-  	#add_column :clients, :domicilio_fiscal, :string
-  	#add_column :clients, :pagina_web, :string
-  	#add_column :clients, :representante_legal, :string
-  	#add_column :clients, :correo, :string
-  	#add_column :clients, :telefono, :string
-  	#add_column :clients, :oficina_actual, :boolean
+  	add_column :clients, :nombre_comercial, :string
+  	add_column :clients, :registro_fiscal, :string
+  	add_column :clients, :domicilio_fiscal, :string
+  	add_column :clients, :pagina_web, :string
+  	add_column :clients, :representante_legal, :string
+  	add_column :clients, :correo, :string
+  	add_column :clients, :telefono, :string
+  	add_column :clients, :oficina_actual, :boolean
   	add_reference :clients, :direccion, index: true
   	add_foreign_key :clients, :direccions
   end
