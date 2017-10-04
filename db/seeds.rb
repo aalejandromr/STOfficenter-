5c30f6c79af741dbc825e33a6049c3898dab4e16
Alejandro Mejia <alejandromejia@Alejandros-MacBook-Air.local>
2017-10-04 06:52:15 -0600

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+TipoDocumento.create!([
+  {tipoDocumento: "DUI"},
+  {tipoDocumento: "NRC"}
+])
+
+Oficina.create!([
+  {name: "CENTRO SAN FRANCISCO", direccion: "Colonia San Francisco,  Calle Los Bambúes y Calle Los Abetos No. 37 A y B.  \nSan Salvador, El Salvador.  \n"},
+  {name: "CENTRO SAN BENITO", direccion: "Colonia San Benito, Calle la Mascota #533, Edificio 533 primer nivel, \n San Salvador, El Salvador.\n"},
+  {name: "CENTRO ESCALON", direccion: "Colonia Escalón,  Calle El Mirador, #4814, edificio  Vittoria quinto nivel, San Salvador, El Salvador. \n"},
+  {name: "CENTRO LA GRAN VIA", direccion: nil}
+])
+
+PeriodoFacturacion.create!([
+  {periodo: "6"},
+  {periodo: "12"},
+  {periodo: "18"},
+  {periodo: "24"}
+])
+
 TipoContrato.create!([
   {tipoContrato: "FISICO"},
   {tipoContrato: "VIRTUAL"},
